4334dba898e122fb3c79c0e8b1755225bf194926
ehdrb92 <kcj920407@gmail.com>
2022-08-03 18:14:51 +0900

    Add : kakao social login, test code

---
+++ core/utils/kakao_api.py
@@ -0,0 +1,39 @@
+import requests
+
+class KakaoAPI:
+    def __init__(self):
+        self.token_api     = "https://kauth.kakao.com/oauth/token"
+        self.user_info_api = "https://kapi.kakao.com/v2/user/me"
+        self.redirect_uri  = redirect_uri
+        self.client_id     = client_id
+
+    def get_kakao_access_token(self, code):
+        body = {
+            'grant_type'  : 'authorization_code',
+            'client_id'   : self.client_id,
+            'redirect_uri': self.redirect_uri,
+            'code'        : code
+            }
+        token_info_res = requests.post(self.token_api, data=body).json()
+        kakao_token    = token_info_res.get('access_token')
+
+        if not kakao_token:
+            raise Exception('access_token not found')
+        
+        return kakao_token
+
+    def get_user_kakao_information(self, kakao_token):
+        headers = {
+            'AUTHORIZATION' : f'Bearer {kakao_token}'
+            }
+        
+        user_kakao_info_res = requests.get(self.user_info_api, headers=headers).json()
+
+        user_kakao_info = {
+            'kakao_id'         : user_kakao_info_res['id'],
+            'email'            : user_kakao_info_res['kakao_account']['email'],
+            'nickname'         : user_kakao_info_res['kakao_account']['profile']['nickname'],
+            'profile_image_url': user_kakao_info_res['kakao_account']['profile']['profile_image_url'],
+        }
+
+        return user_kakao_info
---
+++ core/utils/login_decorator.py
@@ -0,0 +1,31 @@
+import jwt
+
+from django.conf  import settings
+from django.http  import JsonResponse
+
+from users.models import User
+
+def login_decorator(func):
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('AUTHORIZATION')
+
+            if access_token == '':
+                request.user = None
+                return func(self, request, *args, **kwargs)
+
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+            request.user = User.objects.get(id=payload['id'])
+
+            return func(self, request, *args, **kwargs)
+
+        except User.DoesNotExist:
+            return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'MESSAGE':'INVALID_PAYLOAD'}, status=401)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+    
+    return wrapper
