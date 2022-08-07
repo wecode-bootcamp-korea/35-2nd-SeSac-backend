fb8806125114b2990918079796d310d172167670
seoyun <dhn04100@gmail.com>
2022-08-08 02:06:49 +0900

    post POST API 구현

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+import boto3
 
-# Create your views here.
+from urllib.parse import unquote
+from django.http  import JsonResponse
+from django.views import View
+
+from my_settings  import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID
+from users.utils  import login_decorator
+from .models      import Post, PostCategory, Category, PostHashtag, Hashtag, Location, Image
+
+class PostView(View):
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id     = AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY
+    )
+    @login_decorator
+    def post(self, request):
+        try:
+            if request.user == None:
+                return JsonResponse({'message' : 'UNAUTHORIZED_USER'}, status=403)
+
+            data       = request.POST
+            categories = data['categories'].split(',')[:-1]
+            hashtags   = data['hashtags'].split(',')[:-1]
+            files      = request.FILES.getlist('image')
+
+            location = Location.objects.create(
+                address   = data['address'],
+                latitude  = data['latitude'],
+                longitude = data['longitude']
+            )
+
+            post = Post.objects.create(
+                user_id     = request.user.id,
+                location_id = location.id,
+                title       = data['title'],
+                content     = data['content']
+            )
+
+            for file in files:
+                self.s3_client.upload_fileobj(
+                    file, 
+                    "freshus",
+                    unquote(file.name),
+                    ExtraArgs={
+                        "ContentType": file.content_type
+                    }
+                )
+                Image.objects.create(
+                    post_id = post.id,
+                    image_url = "https://freshus.s3.ap-northeast-2.amazonaws.com/%s" %  (file.name)
+                )
+
+            for category in categories:
+                PostCategory.objects.get_or_create(
+                    post_id     = post.id,
+                    category_id = Category.objects.get(name=category).id
+                )
+
+            for hashtag in hashtags:
+                hashtag, created = Hashtag.objects.get_or_create(
+                    name = hashtag
+                )
+                PostHashtag.objects.get_or_create(
+                    post_id     = post.id,
+                    hashtag_id = hashtag.id
+                )
+            return JsonResponse({"message": "CREATED"}, status=201)
+
+        except Category.DoesNotExist:
+            return JsonResponse({'message': 'CATEGORY_DOES_NOT_EXIST'}, status=400)
+        
+        except KeyError:
+            return JsonResponse({"message" : "KEY_ERROR"}, status=400)
+
