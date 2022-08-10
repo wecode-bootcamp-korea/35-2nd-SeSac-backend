c261cc598f45dec728edc29543c8c53048b79f1f
jongbeom <jongbeom@ip-10-58-4-137.ap-northeast-2.compute.internal>
2022-08-10 11:11:54 +0900

    core_review/post-GET

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import PostView
+from .views import PostDetailView
 
 urlpatterns = [
-    path('/<int:post_id>', PostView.as_view())
+    path('/<int:post_id>', PostDetailView.as_view())
 ]
--- posts/views.py
+++ posts/views.py
@@ -3,24 +3,39 @@ import boto3
 from django.http  import JsonResponse
 from django.views import View
 
-from fresh_us.settings import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID
+from fresh_us.settings  import AWS_SECRET_ACCESS_KEY, AWS_ACCESS_KEY_ID
 from users.utils        import login_decorator
 from .models            import Post, Image, Hashtag, Category
 from users.models       import User
-class PostView(View):
-    @login_decorator
+from decorators         import query_debugger
+
+class PostDetailView(View):
+    '''
+    1. id 값 같이 반환
+    2. orm 최적화
+    '''
+    @query_debugger
+    # @login_decorator
     def get(self, request, post_id):
         try:
-            if request.user == None:
-                return JsonResponse({'message' : 'UNAUTHORIZED_USER'}, status=403)
+            # if request.user == None:
+            #     return JsonResponse({'message' : 'UNAUTHORIZED_USER'}, status=403)
             
-            user_id = User.objects.get(id=request.user.id).id
-            post    = Post.objects.get(id=post_id)
+            # user_id = User.objects.get(id=request.user.id).id
+            post    = Post.objects.select_related('user', 'location')\
+                                  .prefetch_related('category')\
+                                  .get(id=post_id)
+
+                                #   .get(id=post_id)
             
             result  = {
-                "user_id"          : user_id,
+                "user_id"          : 1,
                 "post_id"          : post_id,
-                "categories"       : [category.name for category in Category.objects.filter(postcategory__post_id=post_id)],
+                "categories"       : [{
+                    'id'   : category.id,
+                    'name' : category.name 
+                    # } for category in Category.objects.filter(postcategory__post_id=post_id)],
+                    } for category in post.category.all()],
                 "written_user_id"  : post.user.id,
                 "written_user_name": post.user.nickname,
                 "location"         : post.location.address,
